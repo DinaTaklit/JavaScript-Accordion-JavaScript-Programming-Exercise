73f21996ab04d19aa6ca78e9d2b51050871f7d3d
DinaTaklit <ez_taklit@esi.dz>
2021-03-15 13:32:52 +0100

    Finishing Touches

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 const accordion = document.querySelectorAll('.panel');
 console.log(accordion);
+
 accordion.forEach((element) => {
     // console.log(element);
-    element.addEventListener('click', toggleElement)
+    element.addEventListener('click', toggleElement);
+    element.addEventListener('mouseenter', addStyle1);
+    element.addEventListener('mouseover', addStyle2);
 });
 
 
- function toggleElement(e) {
+// Style Functions
+function addStyle1 (e) {
+    e.target.style.color = "red";
+    setTimeout(function(){
+        e.target.style.color ='';
+    }, 500)
+}
+
+function addStyle2(e){
+    e.target.style.color  = 'orange';
+    setTimeout(function(){
+        e.target.style.color ='';
+    }, 500)
+}
+
+
+
+function toggleElement(e) {
     // console.log(e); //display the event
     // console.log(this); // display the parent element panel
     //console.log(e.target); // display the clicked element if the head the head if the body will /displau the body
