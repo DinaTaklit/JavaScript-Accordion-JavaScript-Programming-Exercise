4064e5e3ae35b5378ad757c20e0fc37406c8a4aa
DinaTaklit <ez_taklit@esi.dz>
2021-03-15 12:48:45 +0100

    Hide all open panels

--- script.js
+++ script.js
@@ -9,7 +9,13 @@ accordion.forEach((element) => {
  function toggleElement(e) {
     // console.log(e); //display the event
     // console.log(this); // display the parent element panel
-    //console.log(e.target); // display the clicked element if the head the head if the body will displau the body
-    this.classList.toggle("active");
+    //console.log(e.target); // display the clicked element if the head the head if the body will /displau the body
+    //  Hide all open panels => The long way
+    /*  accordion.forEach(function(ele){
+        ele.classList.remove('active');
+    })*/
+    //  Hide all open panels => The short way
+    const allActivpanels = document.querySelectorAll('.active');
+    allActivpanels.forEach(element => element.classList.remove('active'));
+    this.classList.toggle("active"); // Toggle Active Class
 }
-
