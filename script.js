c3c16cdf708f961835023f683fe0c22ca9510dfe
DinaTaklit <ez_taklit@esi.dz>
2021-03-15 12:32:18 +0100

    Accordion JavaScript Event

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const accordion = document.querySelectorAll('.panel');
 console.log(accordion);
-accordion.forEach((item) => {
-     console.log(item);
+accordion.forEach((element) => {
+    // console.log(element);
+    element.addEventListener('click', toggleElement)
 });
+
+
+ function toggleElement(e) {
+    // console.log(e); //display the event
+    // console.log(this); // display the parent element panel
+    console.log(e.target); // display the clicked element if the head the head if the body will displau the body
+}
+
